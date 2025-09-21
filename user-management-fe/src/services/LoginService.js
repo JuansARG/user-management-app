ae142d48c2077309915ab7d586b0f2db3d06954d
Juan Sarmiento <juan.sarmiento@getronics.com>
2025-09-21 20:50:46 -0300

    est: verificar lint-staged y husky

--- user-management-fe/src/services/LoginService.js
+++ user-management-fe/src/services/LoginService.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import env from '../env';
 
 class LoginService {
+    unusedVariable = 'test';
+
     async login(email, password) {
         const { data } = await axios.post(`${env.BACKEND_URL}/api/login`, {
             email,
